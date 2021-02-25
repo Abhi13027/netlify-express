da284e4b09a4fdfff9bcd758512a174ab86dc1e3
Abhishek Jain <abhijain@Abhisheks-MacBook-Pro.local>
2021-02-25 18:17:33 +0530

    Bug Fix: Minor Fixes

--- src/server.js
+++ src/server.js
@@ -3,12 +3,12 @@ const serverless = require("serverless-http");
 const students = require("../students");
 const app = express();
 
-const router = expres.Router();
+const router = express.Router();
 
 router.get("/students", (req, res) => {
   res.json(students);
 });
 
-app.use("/.netlify/functions/api", router);
+app.use("/.netlify/functions/server", router);
 
 module.exports.handler = serverless(app);
